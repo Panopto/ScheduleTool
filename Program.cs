ac24a75b5f16a01ddcf1d02aba66039569c24069
Bertrand Lee <blee@panopto.com>
2012-10-31 12:03:25 -0700

    Added ScheduleTool sample

---
+++ Program.cs
@@ -0,0 +1,201 @@
+﻿/// <copyright file="Program.cs" company="Panopto Inc.">
+/// Copyright (c) 2012 All Rights Reserved
+/// </copyright>
+/// <summary>
+/// Sample C# client that uses the Panopto PublicAPI
+/// This sample builds a console app that connects to the server via the PublicAPI to schedule
+/// back-to-back recordings for the Panopto Remote Recorder
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+using ScheduleTool.RemoteRecorderManagementService;
+using ScheduleTool.SessionManagementService;
+
+
+namespace ScheduleTool
+{
+    class Program
+    {
+        static bool hasBeenInitialized = false;
+
+        // Command line args        
+        static string user;
+        static string password;
+        static string folderName;
+        static string rrName;
+        static int recordingMins;
+        static int numRecordings;
+
+        const int numArguments = 6;
+
+        /// <summary>
+        /// Print command line usage
+        /// </summary>
+        static void Usage()
+        {
+            Console.WriteLine("ScheduleTool -u [user] -p [password] -f [folder] -r [rr description] -l [recording length mins] -n [number of recordings]");
+        }
+
+        /// <summary>
+        /// Parse command line arguments
+        /// </summary>
+        static bool ParseArgs(string[] args)
+        {
+            bool done = false;
+
+            if (args.Length == (numArguments * 2))
+            {
+                for (int i = 0; i < args.Length && !done; i += 2)
+                {
+                    if (args[i].Length != 2 ||
+                        args[i][0] != '-' ||
+                        i + 1 >= args.Length)
+                    {
+                        break;
+                    }
+
+                    switch (args[i][1])
+                    {
+                        case 'u':
+                            user = args[i + 1];
+                            break;
+
+                        case 'p':
+                            password = args[i + 1];
+                            break;
+
+                        case 'f':
+                            folderName = args[i + 1];
+                            break;
+
+                        case 'r':
+                            rrName = args[i + 1];
+                            break;
+
+                        case 'l':
+                            recordingMins = Int32.Parse(args[i + 1]);
+                            break;
+
+                        case 'n':
+                            numRecordings = Int32.Parse(args[i + 1]);
+                            break;
+
+                        default:
+                            done = true;
+                            break;
+                    }
+                }
+            }
+
+            return  user != null &&
+                    password != null &&
+                    folderName != null &&
+                    rrName != null &&
+                    recordingMins != 0 &&
+                    numRecordings != 0;
+        }
+
+        static void Assert(bool condition, string errorMsg)
+        {
+            if (!condition)
+            {
+                throw new Exception(errorMsg);
+            }
+        }
+
+        /// <summary>
+        /// Ensures that our custom certificate validation has been applied
+        /// </summary>
+        public static void EnsureCertificateValidation()
+        {        
+            if (!hasBeenInitialized)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += new System.Net.Security.RemoteCertificateValidationCallback(CustomCertificateValidation);
+                hasBeenInitialized = true;
+            }
+        }
+
+        /// <summary>
+        /// Ensures that server certificate is authenticated
+        /// </summary>
+        private static bool CustomCertificateValidation(object sender, X509Certificate cert, X509Chain chain, System.Net.Security.SslPolicyErrors error)
+        {         
+            return true;
+        }
+
+
+        /// <summary>
+        /// Main application method
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Parse args
+                if (!ParseArgs(args))
+                {
+                    Usage();
+                    return;
+                }
+
+                // Instantiate service clients
+                ISessionManagement sessionMgr = new SessionManagementClient();
+                IRemoteRecorderManagement rrMgr = new RemoteRecorderManagementClient();
+
+                // Ensure server certificate is validated
+                EnsureCertificateValidation();
+
+                // Create auth info. For this sample, we will be passing auth info into all of the PublicAPI web service calls
+                // instead of using IAuth.LogonWithPassword() (which is the alternative method).
+                SessionManagementService.AuthenticationInfo sessionAuthInfo = new SessionManagementService.AuthenticationInfo()
+                                                                              {
+                                                                                  UserKey = user,
+                                                                                  Password = password
+                                                                              };
+
+                RemoteRecorderManagementService.AuthenticationInfo rrAuthInfo = new RemoteRecorderManagementService.AuthenticationInfo()
+                                                                                {
+                                                                                    UserKey = user,
+                                                                                    Password = password
+                                                                                };
+
+                // Find folder 
+                ScheduleTool.SessionManagementService.Pagination pagination = new ScheduleTool.SessionManagementService.Pagination { MaxNumberResults = int.MaxValue, PageNumber = 0 };
+                ListFoldersResponse response = sessionMgr.GetFoldersList(sessionAuthInfo, new ListFoldersRequest { Pagination = pagination }, null);
+                Folder folder = response.Results.FirstOrDefault(a => a.Name == folderName);
+                Assert(folder != null, "Could not find specified folder - " + folderName);
+
+                // Find RR
+                ScheduleTool.RemoteRecorderManagementService.Pagination rrPagination = new ScheduleTool.RemoteRecorderManagementService.Pagination { MaxNumberResults = int.MaxValue, PageNumber = 0 };
+                ListRecordersResponse rrResponse = rrMgr.ListRecorders(rrAuthInfo, rrPagination, RecorderSortField.Name);
+                RemoteRecorder recorder = rrResponse.PagedResults.FirstOrDefault(a => a.Name == rrName);
+                Assert(recorder != null, "Could not find specified recorder - " + rrName);
+                List<ScheduleTool.RemoteRecorderManagementService.RecorderSettings> recorderSettings = new List<RecorderSettings>();
+                recorderSettings.Add(new RecorderSettings { RecorderId = recorder.Id });                
+
+                // Schedule back-to-back recordings
+                DateTime timeBase = DateTime.UtcNow;
+
+                for (int i = 0; i < numRecordings; i++)
+                {
+                    DateTime startTime = timeBase + TimeSpan.FromMinutes(recordingMins * i);
+                    DateTime endTime = timeBase + TimeSpan.FromMinutes(recordingMins * (i + 1));
+                    ScheduledRecordingResult result = rrMgr.ScheduleRecording(rrAuthInfo, "TestRecording" + (i+1).ToString(), folder.Id, false, startTime, endTime, recorderSettings.ToArray());
+                    Assert(!result.ConflictsExist, "Unable to schedule test recording due to conflicts");
+                }
+
+                Console.WriteLine("Recordings have been scheduled");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,17 @@
+ScheduleTool notes
+------------------
+
+This tool uses the Panopto PublicAPI to schedule back-to-back test recordings for the Panopto Remote Recorder (RR).
+
+Build steps:
+- Open VS2010 solution
+- In Solution Explorer, under Service References, select "Configure Service Reference" for both services
+- Update Address field to point to the correct server, click OK.
+- Build project
+
+Usage steps:
+- Setup a RR client to connect to a RR server
+- Edit ScheduleTool.exe.config and ensure the endpoint addresses point to your server
+- Run "ScheduleTool" without any arguments to see the argument list.
+- This tool does not support spaces in the folder and RR names
+- The back-to-back recordings are scheduled starting from the time the tool is run
